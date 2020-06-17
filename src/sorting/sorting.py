f110d7450bfacda36cbabb5a03f0362308ed86c4
Tanner Williams <tannerwill756@gmail.com>
2020-06-17 12:51:19 -0400

    mvp

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,37 +4,39 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-    i = 0
-    j = 0
-    k = 0
-    while len(arrA) > 0 and len(arrB) > 0:
-        if arrA[i] < arrB[j]:
-            merged_arr[k] = arrA[i]
-            k = k + 1
-            i = i + 1
+    # start pointers at the start of both lists
+    a, b = 0, 0
+
+    # loop through our merged_arr
+    for i in range(len(merged_arr)):
+        # compare the values at both pointers
+        # whichever value is smaller we copy that value to our merged list
+        # increment that pointer
+        # check fi a pointer is out of bounds of its respective array
+        # if it is, then we copy over every element form the other array
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+
+        # both indices are still in bounds of their respective arrays
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
         else:
-            merged_arr[k] = arrB[j]
-            k = k + 1
-            j = j + 1
+            merged_arr[i] = arrB[b]
+            b += 1
 
-    while 0 < len(arrA):
-        merged_arr[k] = arrA[i]
-        k = k + 1
-        i = i + 1
-
-    while 0 < len(arrB):
-        merged_arr[k] = arrA[j]
-        k = k + 1
-        j = j + 1
-
-    print(merged_arr)
+    return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 
 
-arrA = [5, 7, 9, 11, 13]
-arrB = [2, 6, 10, 12, 15]
-merge(arrA, arrB)
+# arrA = [5, 7, 9, 11, 13]
+# arrB = [2, 6, 10, 12, 15]
+# merge(arrA, arrB)
 
 
 def merge_sort(arr):
